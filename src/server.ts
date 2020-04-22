5a878075a8231d4b1593645a45649b319890f2ca
Arthur Castro <arthurcastroes@gmail.com>
2020-04-22 09:08:00 -0300

    feat: config of eslint, ts-dev-server and editorconfig

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-import express from "express";
+import express from 'express';
+
+const PORT = 3333;
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello world' });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on PORT ${PORT}.`);
+});
