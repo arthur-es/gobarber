8c17b2ffda68750d18ae5798734f7e3258115bec
Arthur Castro <arthurcastroes@gmail.com>
2020-05-03 14:55:17 -0300

    feat: upload images

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes/routes';
+import uploadConfig from './config/upload';
 import './database';
 
 const PORT = 3333;
 const app = express();
 
+app.use('/files', express.static(uploadConfig.directory));
 app.use(express.json());
 app.use(routes);
 
